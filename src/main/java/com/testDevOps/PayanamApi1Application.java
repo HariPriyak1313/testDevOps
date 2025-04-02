12177cf6c6762c87a332ca4c977e51a88308a091
HariPriya <kesavanp136@gmail.com>
2025-04-02 11:45:43 +0530

    Initial commit

---
+++ src/main/java/com/testDevOps/PayanamApi1Application.java
@@ -0,0 +1,13 @@
+package com.testDevOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayanamApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayanamApi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/testDevOps/PayanamApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testDevOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayanamApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
